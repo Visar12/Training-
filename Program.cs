c510b48892e7d3bd02b9a6bcbfa5b2581a91521b
visha <visha@TEC-T-PC05>
2022-09-05 14:53:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int number = 3;
+            //while (number > 0)
+            //{
+            //    Console.WriteLine(number);
+            //    number--;   
+            //    {
+
+
+            //int x = 1;
+            //while(x<5)
+            //{
+
+            //        x += 2;
+
+            //    Console.WriteLine(x);
+            //}
+
+            /*Anna's hummingbird, now believed to be the fastest bird in the world relative to its size, can reach speeds of 80 km per hour.
+
+            Task
+            Write a program to output how many kilometers it will travel each hour over 5 hours of flight.
+
+            Output
+            80
+            160
+              240
+             320
+           400
+            */
+
+            //for (int x = 1; x <= 5; x++)
+            //{
+            //    Console.WriteLine(x * 80);
+            //}
+
+            // Numri shkon  bahet 108,6,4,3,2,0 ose muj me bo me shku  0,2,4,6,8,10 me +=
+     //for(int x = 10; x >=0; x -= 2)
+     //       {
+     //           Console.WriteLine(x);
+     //       }
+               
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
